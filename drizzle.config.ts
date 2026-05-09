e4f05757e96d72121a589826876c0e18dd20ba6b
Kevin Dang <dangkevin12@gmail.com>
2026-05-08 21:49:29 -0400

    Load Next env for Drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,7 @@
+import { loadEnvConfig } from "@next/env";
 import { defineConfig } from "drizzle-kit";
+
+loadEnvConfig(process.cwd());
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is required to run Drizzle commands.");
